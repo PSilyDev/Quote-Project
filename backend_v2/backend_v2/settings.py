0d2c1669a09c90db3ba6a654ddd01b00613cdd8c
PSilyDev <codes.prakhar@gmail.com>
2025-04-23 02:38:30 +0530

    final tweaks before hosting

--- backend_v2/backend_v2/settings.py
+++ backend_v2/backend_v2/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".railway.app",
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -88,10 +92,21 @@ WSGI_APPLICATION = 'backend_v2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'shortline.proxy.rlwy.net',
+        'PORT': '14267',
     }
 }
 
